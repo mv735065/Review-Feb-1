b2f59467e83e8f760d4f10ce0561e2b2dcaa63cc
mv735065 <245119735065@mvsrec.edu.in>
2025-02-15 12:27:48 +0530

    4 is completed

---
+++ fourth-drill.js
@@ -0,0 +1,41 @@
+// **Write a function that returns the youngest and oldest person in each profession.**
+
+// { name: "Maria", age: 25, profession: "Doctor", salary: 74000 },
+
+const data=require('./Data/people');
+
+
+let oldAndYoung=getOldAndYoungestPerProfession(data);
+
+function getOldAndYoungestPerProfession(data){
+
+  return  data.reduce((acc,person)=>{
+        let profession=person.profession;
+        let age=person.age;
+        let name=person.name;
+       console.log(profession);
+       
+        if(!acc.hasOwnProperty(profession)){
+            acc[profession]={
+                oldestPerson:"",
+                oldestAge:0,
+                youngestPerson:"",
+                youngetAge:200,
+            }
+        }
+       
+        if(age>acc[profession].oldestAge){
+            acc[profession].oldestAge=age;
+            acc[profession].oldestPerson=name;
+        }
+        else if(age<acc[profession].oldestAge && age<acc[profession].youngetAge ){
+            acc[profession].youngetAge=age;
+            acc[profession].youngestPerson=name;
+        }
+
+    return acc;
+    },{})
+};
+
+console.log(oldAndYoung);
+
